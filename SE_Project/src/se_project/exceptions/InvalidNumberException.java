37d023a92b18b7a3a90b2bfc97cda86c0cf42a7d
aless <aless@LAPTOP-1NQ06NKM.mshome.net>
2021-11-23 17:18:47 +0100

    Aggiunta Eccezioni

---
+++ SE_Project/src/se_project/exceptions/InvalidNumberException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se_project.exceptions;
+
+/**
+ *
+ * @author aless
+ */
+public class InvalidNumberException extends Exception {
+    
+}
