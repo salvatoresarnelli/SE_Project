649a6056357d4274d1be1157e054f6a9e359842b
salvatoresarnelli <s.sarnelli1@studenti.unisa.it>
2021-11-26 19:05:01 +0100

    Test metodo aggiuntivo  Stack

--- SE_Project/src/se_project/InterfacciaController.java
+++ SE_Project/src/se_project/InterfacciaController.java
@@ -75,7 +75,6 @@ public class InterfacciaController implements Initializable {
             n = solver.resolveOperation(text);
             observableList.add(n);
         }
-        
         listView.itemsProperty().bind(listProperty);
         textArea.clear();         
     }
