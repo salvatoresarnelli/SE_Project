ad365658907f6e5acc807c228e7f1af5bb303603
salvatoresarnelli <s.sarnelli1@studenti.unisa.it>
2021-11-27 16:04:45 +0100

    Aggiunta Javadoc alle classi Stack e ComplexNumber

--- SE_Project/src/se_project/ComplexNumber.java
+++ SE_Project/src/se_project/ComplexNumber.java
@@ -7,78 +7,163 @@ package se_project;
 
 import java.lang.Math;
 import other.Utilities;
+import se_project.exceptions.InvalidNumberException;
 import se_project.exceptions.UndefinedPhaseException;
 
 /**
  *
- * @author aless
+ * @author pio
  */
 public class ComplexNumber {
     
     private double a;
     private double b;
+    
+    /**
+    * Metodo costruttore della classe ComplexNumber.
+    * @author pio
+    * @param       double a, double b
+    * @return      Numero complesso con parte reale e immaginaria.
+    */
 
     public ComplexNumber(double a, double b) {
         this.a = a;
         this.b = b;
     }
+    
+    /**
+    * Metodo costruttore della classe ComplexNumber in caso di assenza di parametri.
+    * @author pio
+    * @param       
+    * @return      
+    */
 
     public ComplexNumber() {
         a=0;
         b=0;
     }
+    
+    /**
+    * Il metodo si occupa di settare la parte reale di un numero complesso.
+    * @author pio
+    * @param       double a
+    * @return      
+    */
 
     public void setRealPart(double a) {
         this.a = a;
     }
+    
+    /**
+    * Il metodo si occupa di settare la parte immaginaria di un numero complesso.
+    * @author pio
+    * @param       double b
+    * @return      
+    */
 
     public void setImaginaryPart(double b) {
         this.b = b;
     }
+    
+    /**
+    * Il metodo si occupa di ricavare la parte reale di un numero complesso.
+    * @author pio
+    * @param       
+    * @return parte reale del numero complesso.
+    */
 
     public double getRealPart() {
         return a;
     }
+    
+    /**
+    * Il metodo si occupa di ricavare la parte immaginaria di un numero complesso.
+    * @author pio
+    * @param       
+    * @return parte immaginaria del numero complesso.
+    */
 
     public double getImaginaryPart() {
         return b;
     }
     
+    /**
+    * Il metodo si occupa di ricavare il modulo di un numero complesso.
+    * @author pio
+    * @param       
+    * @return modulo del numero complesso.
+    */
+    
     public double getModule(){
-    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
     
-     public double getPhase() throws UndefinedPhaseException{
-     if (a==0 && b==0)
-         throw new UndefinedPhaseException();
-     if(a<0 && b<0){
-         return Math.atan(b/a)-Math.PI;
-     }
-     if(a<0 && b>=0){
-         return Math.atan(b/a)+Math.PI;
-     }
-     if(a>0){
-        return Math.atan(this.b/this.a);
-     }
+    /**
+    * Il metodo si occupa di ricavare la fase di un numero complesso.
+    * @author pio
+    * @param       
+    * @return fase del numero complesso.
+    * @throws UndefinedPahseException se il numero complesso ha 
+    *         parte reale e immaginaria nulla.
+    * 
+    */
+    
+    public double getPhase() throws UndefinedPhaseException{
+        if (a==0 && b==0)
+            throw new UndefinedPhaseException();
+        if(a<0 && b<0){
+            return Math.atan(b/a)-Math.PI;
+        }  
+        if(a<0 && b>=0){
+            return Math.atan(b/a)+Math.PI;
+        }
+        if(a>0){
+            return Math.atan(this.b/this.a);
+        }
      
-     if(a==0 && b< 0){
-     return -Math.PI/2;}
-     if(a==0 && b>0){
-         return Math.PI/2;
-     }
-     return -1;
+        if(a==0 && b< 0){
+            return -Math.PI/2;
+        }
+        if(a==0 && b>0){
+            return Math.PI/2;
+        }
+        return -1;
     }
+    
+    /**
+    * Il metodo si occupa di trasformare il numero complesso in forma trigonometrica
+    * in un numero complesso in forma cartesiana. 
+    * @author pio
+    * @param  double mod, double phase
+    * @return numero complesso in forma cartesiana.
+    */
  
-     public static ComplexNumber fromTrigonometric(double mod, double phase){
-         double newA = mod * Math.cos(phase);
-         double newB = mod * Math.sin(phase);
-         return new ComplexNumber(Utilities.round(newA,3),Utilities.round(newB,3));
+    public static ComplexNumber fromTrigonometric(double mod, double phase){
+        double newA = mod * Math.cos(phase);
+        double newB = mod * Math.sin(phase);
+        return new ComplexNumber(Utilities.round(newA,3),Utilities.round(newB,3));
     }
+    
+    /**
+    * Il metodo si occupa di calcolare il coniugato di un numero compesso.
+    * @author pio
+    * @param ComplexNumber n
+    * @return congiugato del numero complesso.
+    * @throws InvalidNumerException se il numero complesso è null.
+    */
      
-     public ComplexNumber conjugated(ComplexNumber n){
-         n.b+=-2*n.b;
-         return n;
-     }
+    public ComplexNumber conjugated(ComplexNumber n) throws InvalidNumberException{
+        if(n == null) throw new InvalidNumberException();
+        n.b+=-2*n.b;
+        return n;
+    }
+    
+    /**
+    * Il metodo fornisce un codice hash
+    * @author pio
+    * @param       
+    * @return numero intero corrispondete al codice hash
+    */
 
     @Override
     public int hashCode() {
@@ -86,6 +171,13 @@ public class ComplexNumber {
         return hash;
     }
 
+    /**
+    * Il metodo si occupa verificare se un numero complesso è uguale ad un oggetto.
+    * @author pio
+    * @param Object obj
+    * @return true, se il numero complesso è uguale ad un altro oggetto, 
+    *         false altrimenti. 
+    */
    
     @Override
     public boolean equals(Object obj) {
@@ -107,6 +199,13 @@ public class ComplexNumber {
         }
         return true;
     }
+    
+    /**
+    * Il metodo si occupa di effettuare la stampa di un numero complesso.
+    * @author pio
+    * @param       
+    * @return stringa che corrisponde al numero complesso.
+    */
 
     @Override
     public String toString() {
@@ -117,8 +216,6 @@ public class ComplexNumber {
         sb.append("j");
         return sb.toString();
     }
-     
-     
-     
+ 
 }
 
--- SE_Project/src/se_project/Stack.java
+++ SE_Project/src/se_project/Stack.java
@@ -15,27 +15,67 @@ import javafx.collections.ObservableList;
  * @author Salvatore Sarnelli
  */
 
-//TO DO: aggiornare con Complex number quando c'è il toString in ComplexNumber
 public class Stack {
     private LinkedList<ComplexNumber> stack;
+    
+    /**
+     * Metodo costruttore della classe Stack.
+     * @author Salvatore Sarnelli
+     * @param  
+     * @return      stack
+     * 
+     */
 
     public Stack() {
         stack = new LinkedList<>() ;
     }
+    
+    /**
+    * Il metodo si occupa di ritornare lo stack
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return      
+    */
+
+    public LinkedList<ComplexNumber> getStack() {
+        return stack;
+    }
+    
+    /**
+    * Il metodo si occupa di inserire un nuovo numero 
+    * complesso alla fine dello stack.
+    * @author Salvatore Sarnelli
+    * @param ComplexNumber c , numero complesso da dover inserire.
+    * @return      true se l'inserimento è stato effettuato
+    *              correttamente, false altrimenti.  
+    */
     
     public boolean push(ComplexNumber c) {
         if(c == null) return false;
         stack.addLast(c);
         return true;
     }
-
-    public LinkedList<ComplexNumber> getStack() {
-        return stack;
-    }
+    
+    /**
+    * Il metodo si occupa di restituire la dimensione dello stack.
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return      dimensione dello stack.           
+    */
 
     public int size() {
         return stack.size();
     }
+    
+    /**
+    * Il metodo si occupa di rimuovere un numero complesso dalla fine
+    * dello stack.
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return      numero complesso rimosso dallo stack.    
+    * @throws EmptyStackException se si senta di rimuovere un elemento 
+    *         dallo stack vuoto
+    */
     
     public ComplexNumber pop() throws EmptyStackException{
         if(isEmpty()) throw new EmptyStackException();
@@ -43,19 +83,51 @@ public class Stack {
         return c;
     }
     
+    /**
+    * Il metodo si occupa di leggere il valore dell'ultimo
+    * elemento nello stack, senza rimuoverlo.
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return      numero complesso letto dallo stack. 
+    * @throws EmptyStackException se si senta di rimuovere un elemento 
+    *         dallo stack vuoto.
+    */
+    
     public ComplexNumber peek() throws EmptyStackException {
         if(isEmpty()) throw new EmptyStackException();
         return stack.getLast();
     }
     
+    /**
+    * Il metodo si occupa di verificare se lo stack è vuoto.
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return       true, se lo stack è vuoto, false altrimenti.
+    */
+    
     public boolean isEmpty() {
         return size() == 0;
     }
+    
+    /**
+    * Il metodo si occupa di stampare gli elementi dello stack.
+    * @author Salvatore Sarnelli
+    * @param 
+    * @return      elementi dello stack.
+    */
     
     @Override
     public String toString() {
         return "stack = " + stack;
     }
+    
+    /**
+    * Il metodo si occupa di inserire nello stack una lista di numeri complessi.
+    * @author Salvatore Sarnelli
+    * @param       LinkedList<ComplexNumber> list
+    * @return      true, se gli elementi sono stati inseriti correttamente,
+    *              false altrimenti.
+    */    
     
     public boolean fromListToStack(LinkedList<ComplexNumber> list) {
         if(list == null) return false;
@@ -65,5 +137,4 @@ public class Stack {
         return true;
 
     }
-
 }
