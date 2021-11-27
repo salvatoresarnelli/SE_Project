98ed6f4509e73179d12992c4756c3b55b8bd5dc0
aless <aless@LAPTOP-1NQ06NKM.mshome.net>
2021-11-27 17:44:27 +0100

    Aggiustato inserimento GUI

--- SE_Project/src/se_project/InterfacciaController.java
+++ SE_Project/src/se_project/InterfacciaController.java
@@ -17,11 +17,9 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import se_project.exceptions.DivisionByZeroException;
@@ -36,36 +34,36 @@ import se_project.exceptions.UndefinedPhaseException;
  * @author emanu
  */
 public class InterfacciaController implements Initializable {
-
     @FXML
     private TextArea textArea;
+    @FXML 
+    private TextField inputField;
     @FXML
     private Button buttonPush;
     @FXML
     private ListView<ComplexNumber> listView;
     private final Solver solver = Solver.getInstance();
     private final ParserString parser = new ParserString();
-    private final String operation = "__OPERATION__";
+    private final String operation  = "__OPERATION__";
     private final String complex_number = "__COMPLEX__NUMBER__";
     private final String single_number = "__SINGLENUMBER__";
-    private final String invalid_insert = "__INVALID__";
     private ObservableList<ComplexNumber> observableList;
     protected ListProperty<ComplexNumber> listProperty = new SimpleListProperty<>();
 
     public void initialize(URL url, ResourceBundle rb) {
         observableList = FXCollections.observableList(solver.getStructureStack().getStack());
+
         textArea.setOnKeyPressed((KeyEvent event) -> {
-            if (event.getCode().equals(KeyCode.ENTER)) {
+        if (event.getCode().equals(KeyCode.ENTER)) 
                 buttonPush.fire();
-            }
         });
         listProperty.set(observableList);
     }
 
     @FXML
     private void ActionPush(ActionEvent event) throws NotApplicableOperation, InvalidNumberException, EmptyStackException, UndefinedPhaseException, DivisionByZeroException {
-        String text = textArea.getText();
-        String code = parser.parserString(text);
+        String text = inputField.getText();
+            String code = parser.parserString(text);
         ComplexNumber n;
         if (code.equals(complex_number)) {
             n = parser.recognizeComplexNumber(text);
@@ -73,32 +71,21 @@ public class InterfacciaController implements Initializable {
         }
         if (code.equals(single_number)) {
             n = parser.recognizeNumber(text);
+
             observableList.add(n);
         }
         if (code.equals(operation)) {
-            text = text.replaceAll("\\n", "");
-            if (text.equals("square root") || text.equals("sqrt")) {
-                observableList.addAll(solver.squareRoot());
-            } else {
-                n = solver.resolveOperation(text);
-                observableList.add(n);
-            }
-
-        }
-        if (code.equals(invalid_insert)) {
-
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("Inserimento non valido");
-            alert.setHeaderText("L'elemento inserito non è corretto , riprovare");
-            alert.setContentText(text + " --> L'inserimento non è valido");
-            alert.showAndWait().ifPresent(rs -> {
-                if (rs == ButtonType.OK);
-
-            });
-
+            n = solver.resolveOperation(text);
+            observableList.add(n);
         }
         listView.itemsProperty().bind(listProperty);
-        textArea.clear();
+        inputField.clear();
+        
+
+        
     }
+      @FXML
+    private void ActionReleased(ActionEvent event) throws NotApplicableOperation, InvalidNumberException, EmptyStackException, UndefinedPhaseException, DivisionByZeroException {
+        }
 
 }
