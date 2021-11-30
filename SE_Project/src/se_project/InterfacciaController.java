4cf84e3d3369de4d4820737670d3e58cf42709e8
aless <aless@LAPTOP-1NQ06NKM.mshome.net>
2021-11-30 12:06:31 +0100

    Aggiunta la possibilità di inserire comandi precedenti/successivi premendo freccia su/giu

--- SE_Project/src/se_project/InterfacciaController.java
+++ SE_Project/src/se_project/InterfacciaController.java
@@ -7,11 +7,10 @@ package se_project;
 
 import javafx.scene.control.ListView;
 import java.net.URL;
-import java.util.Collection;
-import java.util.Observable;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
@@ -23,7 +22,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -39,8 +37,12 @@ import se_project.exceptions.UndefinedPhaseException;
  * @author emanu
  */
 public class InterfacciaController implements Initializable {
-    
-    @FXML 
+
+    LinkedList<String> prevs;
+    int index = 0;
+    ListIterator<String> it;
+
+    @FXML
     private TextField inputField;
     @FXML
     private Button buttonPush;
@@ -48,7 +50,7 @@ public class InterfacciaController implements Initializable {
     private ListView<ComplexNumber> listView;
     private final Solver solver = Solver.getInstance();
     private final ParserString parser = new ParserString();
-    private final String operation  = "__OPERATION__";
+    private final String operation = "__OPERATION__";
     private final String complex_number = "__COMPLEX__NUMBER__";
     private final String single_number = "__SINGLENUMBER__";
     private final String invalid_insert = "__INVALID__";
@@ -57,17 +59,57 @@ public class InterfacciaController implements Initializable {
 
     public void initialize(URL url, ResourceBundle rb) {
         observableList = FXCollections.observableList(solver.getStructureStack().getStack());
-
+        prevs = new LinkedList<>();
+        it = prevs.listIterator();
         inputField.setOnKeyPressed((KeyEvent event) -> {
-        if (event.getCode().equals(KeyCode.ENTER)) 
+            String tmp;
+
+            if (event.getCode().equals(KeyCode.ENTER)) {
                 buttonPush.fire();
+            }
+
+            if (event.getCode().equals(KeyCode.UP)) {
+                try {
+                    it = prevs.listIterator(index);
+
+                    tmp = it.previous();
+                    if(!tmp.isEmpty())
+                        inputField.setText(tmp);
+                    index--;
+                } catch (IndexOutOfBoundsException | NoSuchElementException ex) {
+                    index = 0;
+
+                }
+            }
+            if (event.getCode().equals(KeyCode.DOWN)) {
+                    try {
+                    it = prevs.listIterator(index);
+
+                    tmp = it.next();
+                    if(!tmp.isEmpty())
+                        inputField.setText(tmp);
+                    index++;
+                } catch (IndexOutOfBoundsException | NoSuchElementException ex) {
+                    inputField.setText("");
+                    index = prevs.size();
+
+                }
+                
+            }
+
         });
+
         listProperty.set(observableList);
     }
 
     @FXML
     private void ActionPush(ActionEvent event) throws NotApplicableOperation, InvalidNumberException, EmptyStackException, UndefinedPhaseException {
+
         String text = inputField.getText();
+        if (!text.isEmpty()) {
+            prevs.addLast(text);
+            index = prevs.size();
+        }
         String code = parser.parserString(text);
         ComplexNumber n;
         if (code.equals(complex_number)) {
@@ -80,7 +122,7 @@ public class InterfacciaController implements Initializable {
         }
         if (code.equals(operation)) {
             try {
-                if(text.equals("square root") || text.equals("sqrt")) {
+                if (text.equals("square root") || text.equals("sqrt")) {
                     observableList.addAll(solver.squareRoot());
                 } else {
                     n = solver.resolveOperation(text);
@@ -93,20 +135,20 @@ public class InterfacciaController implements Initializable {
                 alert.setHeaderText("Operazione non ammissibile.");
                 alert.setContentText("Non si può dividere per zero.");
                 alert.showAndWait().ifPresent(rs -> {
-                    if (rs == ButtonType.OK);      
+                    if (rs == ButtonType.OK);
                 });
             }
         }
-        if(code.equals(invalid_insert)){
-                Alert alert = new Alert(AlertType.ERROR);
-                alert.setTitle("Inserimento non valido");
-                alert.setHeaderText("L'elemento inserito non è corretto , riprovare");
-                alert.setContentText(text + " --> L'inserimento non è valido");
-                alert.showAndWait().ifPresent(rs -> {
-                    if (rs == ButtonType.OK);
-                });
+        if (code.equals(invalid_insert)) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Inserimento non valido");
+            alert.setHeaderText("L'elemento inserito non è corretto , riprovare");
+            alert.setContentText(text + " --> L'inserimento non è valido");
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK);
+            });
         }
         listView.itemsProperty().bind(listProperty);
-        inputField.clear(); 
+        inputField.clear();
     }
 }
